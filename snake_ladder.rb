c36e1c6ebbdced30c327462531fe469e56449156
Colin Farquhar <cifarquhar@gmail.com>
2017-02-08 14:49:23 +0000

    refactored randomisers

--- snake_ladder.rb
+++ snake_ladder.rb
@@ -25,10 +25,10 @@ class SnakeLadder
 
   def randomize_ladders
       
-      end_counter = 5 #rand(4..14)
+      end_counter = 8 #rand(4..14)
       while @array.count < end_counter
-        start_value = rand(1..19)
-        end_value = start_value + rand(20..80)
+        start_value = rand(1..49)
+        end_value = start_value + rand(1..50)
         ladder = {start: start_value, end: end_value}
        @array.push(ladder) if check_unique_start(ladder,@array) == true
      
@@ -39,10 +39,10 @@ class SnakeLadder
   
   def randomize_snakes
       
-      end_counter = 5 #rand(4..14)
+      end_counter = 8 #rand(4..14)
       while @array.count < end_counter
         start_value = rand(50..99)
-        end_value = start_value + rand(10..49)
+        end_value = start_value - rand(1..49)
         snake = {start: start_value, end: end_value}
        @array.push(snake) if check_unique_start(snake,@array) == true
      
