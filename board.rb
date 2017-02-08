37eb0db0c9927fd9548d49874731c9fb4682f130
Colin Farquhar <cifarquhar@gmail.com>
2017-02-08 10:40:16 +0000

    check for snakes/ladders

--- board.rb
+++ board.rb
@@ -1,16 +1,25 @@
 class Board 
 
-  attr_reader :squares, :snakes
+  attr_reader :squares, :snakes, :ladders
 
   def initialize
-    @squares = Array (0..100)
+    #@squares = Array (0..100)
     @snakes = {start: 26, end: 10}
-
+    @ladders = {start: 3, end: 7}
   end
 
-# def snake move (player)
+  def find_player(player)
+    return player.position
+  end
 
 
+  def check_step(player)
+    if player.position == @snakes[:start] || player.position == @ladders[:start]
+      return true
+    else 
+      return false
+    end
+  end
 
 
 
--- specs/board_spec.rb
+++ specs/board_spec.rb
@@ -17,17 +17,33 @@ class TestBoard < MiniTest::Test
   end
 
 
-  def test_has_spaces
-    array = Array (0..100)
-    assert_equal(array, @board.squares)
-  end
+  # def test_has_spaces
+  #   array = Array (0..100)
+  #   assert_equal(array, @board.squares)
+  # end
 
   def test_has_snakes
     assert_equal(false,@board.snakes.empty?)
-
   end
 
+  def test_find_player_position
+    assert_equal(0,@board.find_player(@player1))
+  end
 
+  # def test_player_moved
+  #   @player1.move
+  #   position = @player1.position
+  #   assert_equal(position, )
+  # end
 
+  def test_check_player_landed_on_snake
+    @player1.position = 26
+    assert_equal( true , @board.check_step(@player1))
+  end
+
+  def test_check_player_landed_on_ladder
+    @player1.position = 3
+    assert_equal( true , @board.check_step(@player1))
+  end
 
 end
